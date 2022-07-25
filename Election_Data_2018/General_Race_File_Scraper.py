8127887ced2c3ff62915abc32177869ece421989
landandair <jonhuwaldt@gmail.com>
2022-07-24 23:02:51 -0500

    Changed File Structure

--- Election_Data_2018/General_Race_File_Scraper.py
+++ Election_Data_2018/General_Race_File_Scraper.py
@@ -1,11 +1,11 @@
 import numpy as np
 import csv
 
-state_list = ('AZ', 'MI', 'OH', 'PA', 'WA')
+state_list = ('AZ', 'MI', 'OH', 'PA', 'WA', 'NC', 'KS')
 state_info = {}
 for state in state_list:
     state_info[state] = []
-with open('SENATE_precinct_general.csv', 'r') as file:
+with open('HOUSE_precinct_general.csv', 'r') as file:
     reader = csv.reader(file)
     for line in reader:
         state_name = line[19]
@@ -13,7 +13,7 @@ with open('SENATE_precinct_general.csv', 'r') as file:
             state_info[state_name].append(line)
 
 for state in state_info.keys():
-    with open('SENATE2018_precinct_' + state + '.csv', 'w') as file:
+    with open('HOUSE2018_precinct_' + state + '.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerows(state_info[state])
         print(state_info[state])
