51f44a1cddcb9f500146de811fdbc88a4bacb99c
landandair <103214142+landandair@users.noreply.github.com>
2022-07-30 21:19:56 -0500

    More Functionality

--- 538_Data/Poll_Data_Scraper.py
+++ 538_Data/Poll_Data_Scraper.py
@@ -13,7 +13,42 @@ def update_polls():
         open(file, 'wb').write(r.content)
 
 
+def get_poll_data(race_dict):
+    """
+    Gets Race Data from airtable
+    Inputs
+        - dict: format: 538 raceid : [Airtable name, 0]
+    outputs
+        - dict: format: 538 raceid : [Airtable name, chance of win]
+    """
+    return race_dict
+
+
 if __name__ == '__main__':
+    # comment out if you don't want new files
     update_polls()
-    races = np.loadtxt('', delimiter=',', dtype='str')
-    desired_races = []
+
+    # Translations(Add more if needed)
+    abbreviations = {'Kansas': 'KA', 'Michigan': 'MI', 'Wisconsin': 'WI', 'PA': 'PA'}
+
+    races = np.loadtxt('Election_List.csv', delimiter=',', dtype='str')
+    desired_races = {}
+    # Translating into 538 Race Designators
+    for race in races:
+        if race.__contains__('Governor'):  # For Governor
+            state = race.split(' ')[0]
+            if abbreviations.keys().__contains__(state):
+                updated_name = abbreviations[state] + '-G1'
+                desired_races[updated_name] = [race, 0]
+            else:
+                print(state + ' Is not in the abbreviations list please add it')
+        elif race.__contains__('U.S.'):  # For Nat. Senate
+            updated_name = race.split(' ')[3] + '-S3'
+            desired_races[updated_name] = [race, 0]
+        elif len(race) == 5:  # For Nat. House
+            updated_name = race[:3] + str(int(race[3:]))
+            desired_races[updated_name] = [race, 0]
+
+    print(desired_races)
+
+
